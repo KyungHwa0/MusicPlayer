5c34ca72e34ce8017b980cd41644e238dc0693aa
kyunghwa <snoopn303@gmail.com>
2024-02-07 20:52:24 +0900

    Fragment연결

--- app/src/main/java/com/wack/musicplayer/view/MainActivity.kt
+++ app/src/main/java/com/wack/musicplayer/view/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.wack.musicplayer
+package com.wack.musicplayer.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wack.musicplayer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frm_container, PlayerFragment.newInstance())
+            .commit()
     }
 }
